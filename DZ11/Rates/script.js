e1426230dee597d3c6f44c4d7dc2cf337a8cd7e0
Svetlana Pavlovskaya <sveta.pavl@tut.by>
2020-10-18 23:15:02 +0300

    dz done

--- DZ11/Rates/script.js
+++
@@ -1,7 +0,0 @@
-if (window.Worker) {
-    const worker = new Worker('worker.js');
-    worker.postMessage('do something');
-    worker.addEventListener('message', function(event) {
-        receiving(event.data);       
-    });   
-}
